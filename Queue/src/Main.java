faae3ff1e281452e5ec58a90cc99c46bd8334a81
darshanborade07 <darshanborade67@gmail.com>
2024-06-22 00:29:29 +0530

    updated

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -2,23 +2,38 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Queue q = new Queue(4);
+//		Queue q = new Queue(4);
+//		
+//		q.insert(10);
+//        System.out.println(q.delete());
+//
+//        System.out.println(q.insert(20));
+//        System.out.println(q.insert(30));
+//        System.out.println(q.insert(40));
+//        System.out.println(q.insert(50));
+//
+//        System.out.println(q.delete());
+//        System.out.println(q.delete());
+//        System.out.println(q.delete());
+//        System.out.println(q.delete());
+//
+//        System.out.println(q.isEmpty() + " " + q.isFull());
+
+        CircularQueue cq = new CircularQueue(5);
+
+        System.out.println(cq.insert(10));
+        System.out.println(cq.insert(20));
+        System.out.println(cq.insert(30));
+        System.out.println(cq.insert(40));
+        System.out.println(cq.insert(50));
+        System.out.println(cq.insert(50));
+
+        System.out.println(cq.delete());
+        System.out.println(cq.insert(60));
+        System.out.println(cq.delete());
+        System.out.println(cq.insert(60));
 		
-		q.insert(10);
-        System.out.println(q.delete());
-
-        System.out.println(q.insert(20));
-        System.out.println(q.insert(30));
-        System.out.println(q.insert(40));
-        System.out.println(q.insert(50));
-
-        System.out.println(q.delete());
-        System.out.println(q.delete());
-        System.out.println(q.delete());
-        System.out.println(q.delete());
-
-        System.out.println(q.isEmpty() + " " + q.isFull());
-
+		
 	}
 
 }
